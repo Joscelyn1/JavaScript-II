06e97594718b07e3e1eddee3e185a3666fbf9452
Joscelyn <JoscelynRose@gmail.com>
2019-06-11 14:32:09 -0400

    finished closure stretch question 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -28,12 +28,17 @@ console.log(minus5(59));
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+function counter() {
+  let number = 0;
+  function add1() {
+    return ++number;
+
+  }
+  return add1;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
